ba9e58f04240c5142defc2b2a510f388fa3428be
Mindey <mindey@qq.com>
2020-12-27 22:33:00 +0000

    update: add v2 of vocabulary, based on interest in -- Categories, Systems, Methods, Operations and Results (Items).

---
+++ nrv/v2.py
@@ -0,0 +1,55 @@
+data = {
+    'category': [
+        'goal',
+        'concept',
+        'question'
+    ],
+    'system': [
+        'project',
+        'structure',
+        'mechanism',
+        'organization',
+        'person',
+        'process',
+        'api'
+    ],
+    'method': [
+        'idea',
+        'principle',
+        'function',
+        'invention',
+        'equipment',
+        'product',
+        'endpoint'
+    ],
+    'operation': [
+        'CATEGORY',
+        'call',
+        'call:query',
+        'order',
+        'order:buy',
+        'order:sell',
+        'order:transfer',
+        'order:machining',
+        'order:shipment',
+        'order:payment',
+        'order:contract',
+        'task',
+        'task:programming',
+        'task:engineering',
+        'task:manufacturing',
+        'task:treatment',
+    ],
+    'item': [
+        'record',
+        'document',
+        'METHOD',
+        'SYSTEM',
+        'OPERATION'
+        'location',
+        'location:address',
+        'location:account',
+        'location:url'
+    ],
+}
+
--- tests/test_validate.py
+++ tests/test_validate.py
@@ -9,12 +9,12 @@ def test_none():
 
 def test_():
     versionless_batch_1 = [
-        '0oo.li/Intent::category',
-        '0oo.li/Method::method',
-        '0oo.li/Project::system:organization',
-        '0oo.li/Result::resource',
-        '0oo.li/Operation::request:order',
-        '0oo.li/Tool::method:equipment',
+        '0oo.li/Intent::v1/category',
+        '0oo.li/Method::v1/method',
+        '0oo.li/Project::v1/system:organization',
+        '0oo.li/Result::v1/resource',
+        '0oo.li/Operation::v1/request:order',
+        '0oo.li/Tool::v1/method:equipment',
     ]
     versioned_batch_2 = [
         '0oo.li/Place::v1/location',
@@ -28,12 +28,16 @@ def test_():
         '0oo.li/Person::v2/system:person'
     ]
     triple_batch_4 = [
-        '0oo.li/Trade::v3/resource:operation:contract',
+        '0oo.li/Trade::v1/resource:operation:contract',
+    ]
+    v2_test_5 = [
+        '0oo.li/Location::v2/item:location',
     ]
 
     b1 = all([validate_name(name) for name in versionless_batch_1])
     b2 = all([validate_name(name) for name in versioned_batch_2])
     b3 = not all([validate_name(name) for name in failure_batch_3])
     b4 = all([validate_name(name) for name in triple_batch_4])
+    b5 = all([validate_name(name) for name in v2_test_5])
 
-    assert(b1 and b2 and b3)
+    assert(b1 and b2 and b3 and b4 and b5)
