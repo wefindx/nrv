d860ab331d87b2496d8646fd38782026b0d06ec0
Mindey <mindey@qq.com>
2020-12-28 01:46:50 +0000

    update: bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name='nrv',
-    version='0.1.1',
+    version='0.1.2',
     description='Network Resource Vocabulary.',
     long_description=long_description,
     url='https://gitlab.com/wefindx/nrv',
